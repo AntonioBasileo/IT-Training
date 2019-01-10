5c0af5b0a7bbe21d23ddd154fcd6a3732ffe32e8
lauraPintu <45484945+lauraPintu@users.noreply.github.com>
2019-01-10 23:25:06 +0100

    checkstyle

--- ittraining/src/main/java/it/unisa/di/ittraining/studente/StudentiService.java
+++ ittraining/src/main/java/it/unisa/di/ittraining/studente/StudentiService.java
@@ -1,12 +1,7 @@
 package it.unisa.di.ittraining.studente;
 
-import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
+import it.unisa.di.ittraining.studente.MatricolaStudenteEsistenteException;
+import it.unisa.di.ittraining.studente.MatricolaStudenteNonValidaException;
 import it.unisa.di.ittraining.studente.Studente;
 import it.unisa.di.ittraining.studente.StudenteRepository;
 import it.unisa.di.ittraining.utente.CognomeNonValidoException;
@@ -24,189 +19,247 @@ import it.unisa.di.ittraining.utente.UsernameEsistenteException;
 import it.unisa.di.ittraining.utente.UsernameNonValidoException;
 import it.unisa.di.ittraining.utente.Utente;
 import it.unisa.di.ittraining.utente.UtenteRepository;
-import it.unisa.di.ittraining.studente.MatricolaStudenteEsistenteException;
-import it.unisa.di.ittraining.studente.MatricolaStudenteNonValidaException;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
 
 @Service
 public class StudentiService {
-	  
-	  @Autowired
-	  private StudenteRepository studenteRepository;
-	  
-	  @Autowired
-	  private UtenteRepository utenteRep;
 
-	  
-	  
-	  /** 
-	   * Costante che rappresenta la minima distanza in anni dalla data corrente 
-	   * per la data di nascita.
-	   */
-	  public static final int MIN_DISTANZA_ANNO_NASCITA = 17;
-		 
-		/** 
-		* Costante che rappresenta la massima distanza in anni dalla data corrente 
-		* per la data di nascita.
-		*/
-	  public static final int MAX_DISTANZA_ANNO_NASCITA = 130;
-	  
-	  @Transactional(rollbackFor = Exception.class)
-	  public Studente registraStudente(Studente studente) throws NomeNonValidoException, NomeCognomeTroppoLungoException, NomeCognomeTroppoCortoException, CognomeNonValidoException,
-	  DataDiNascitaNonValidaException, UsernameNonValidoException, UsernameEsistenteException, EmailNonValidaException, EmailEsistenteException, SessoNonValidoException, TelefonoNonValidoException,
-	  MatricolaStudenteNonValidaException, MatricolaStudenteEsistenteException, PasswordNonValidaException, PasswordNonCorrispondentiException {
-		  
-		  	studente.setNome(validaNome(studente.getNome()));
-		  	studente.setCognome(validaCognome(studente.getCognome()));
-		  	studente.setDataDiNascita(validaDataDiNascita(studente.getDataDiNascita()));
-		  	studente.setUsername(validaUsername(studente.getUsername()));
-		  	studente.setEmail(validaEmailStudente(studente.getEmail()));
-		  	studente.setSesso(validaSesso(studente.getSesso()));
-		  	studente.setTelefono(validaTelefono(studente.getTelefono()));
-		  	studente.setMatricola(validaMatricolaStudente(studente.getMatricola()));
-		  	studente.setPassword(validaPassword(studente.getPassword()));
-		    
-		    studente = studenteRepository.save(studente);
-		    
-		    return studente;
-		  
-	  }
-	  
-	  
-	  public String validaMatricolaStudente(String matricolaStudente)
-		         throws MatricolaStudenteNonValidaException, MatricolaStudenteEsistenteException {
-		    if (matricolaStudente == null) {
-		    	
-		      throw new MatricolaStudenteNonValidaException();
-		      
-		    } else {
-		    	
-		      matricolaStudente = matricolaStudente.trim();
-		      
-		      if (!matricolaStudente.matches(Studente.MATRICOLA_PATTERN)) {
-		        throw new MatricolaStudenteNonValidaException();
-		        
-		      } else if (studenteRepository.existsByMatricola(matricolaStudente)) {
-		        throw new MatricolaStudenteEsistenteException();
-		        
-		      } else {
-		    	  
-		        return matricolaStudente;
-		      }
-		    }
-		  }
-	  
-	  public String validaEmailStudente(String email) throws EmailNonValidaException, EmailEsistenteException {
-		  if(email == null) throw new EmailNonValidaException("Il campo email non può essere nullo");
-		  
-		  if(!email.matches(Studente.EMAIL_PATTERN_STUDENTE)) throw new EmailNonValidaException();
-		  
-		  if(studenteRepository.existsByEmail(email)) throw new EmailEsistenteException("L'email è già utilizzata da un altro studente");
-		  
-		  return email;
-	  }
-	
-	  
-	  
-	  public String validaNome(String nome) throws NomeNonValidoException, NomeCognomeTroppoLungoException, NomeCognomeTroppoCortoException {
-		  if(nome == null) throw new NomeNonValidoException();
-		  
-		  if(nome.length() > Utente.MAX_LUNGHEZZA_NOME) throw new NomeCognomeTroppoLungoException();
-		  
-		  if(nome.length() < Utente.MIN_LUNGHEZZA_NOME) throw new NomeCognomeTroppoCortoException();
-		  
-		  if(!nome.matches(Utente.NOME_PATTERN)) throw new NomeNonValidoException("Il campo nome non rispetta il formato indicato");
-		  
-		  return nome;
-	  }
-	  
-	  
-	  public String validaCognome(String cognome) throws CognomeNonValidoException, NomeCognomeTroppoLungoException, NomeCognomeTroppoCortoException {
-	    if (cognome == null) throw new CognomeNonValidoException();
-		  
-		  if(cognome.length() > Utente.MAX_LUNGHEZZA_NOME) throw new NomeCognomeTroppoLungoException();
-		  
-		  if(cognome.length() < Utente.MIN_LUNGHEZZA_NOME) throw new NomeCognomeTroppoCortoException();
-	    
-	    if(!cognome.matches(Utente.COGNOME_PATTERN)) throw new CognomeNonValidoException("Il cognome non rispetta il formato indicato");
-	    
-	    return cognome;
-	  }
-	
+  @Autowired
+  private StudenteRepository studenteRepository;
+  
+  @Autowired
+  private UtenteRepository utenteRep;
 
-	  public String validaSesso(String sesso) throws SessoNonValidoException {
-		   if (sesso == null) {
-		     throw new SessoNonValidoException();
-		     
-		   } else {
-		     sesso = sesso.trim();
-		      
-		     if (!sesso.equals(Utente.SESSO_MASCHILE)
-		         && !sesso.equals(Utente.SESSO_FEMMINILE)) {
-		       throw new SessoNonValidoException();
-		     } else {
-		       return sesso;
-		     }
-		   }
-		}
-	
+  /** 
+  * Costante che rappresenta la minima distanza in anni dalla data corrente 
+  * per la data di nascita.
+  */
+  public static final int MIN_DISTANZA_ANNO_NASCITA = 17;
 
-	 public LocalDate validaDataDiNascita(LocalDate dataDiNascita) 
-	        throws DataDiNascitaNonValidaException {
-	         
-	    if (dataDiNascita == null) {
-	      throw new DataDiNascitaNonValidaException();
-	      
-	    } else {
-	    	
-	      LocalDate oggi = LocalDate.now();
-	      long distanza = ChronoUnit.YEARS.between(dataDiNascita, oggi);
-	      
-	      if (distanza < MIN_DISTANZA_ANNO_NASCITA || distanza > MAX_DISTANZA_ANNO_NASCITA) {
-	        throw new DataDiNascitaNonValidaException();
-	      } else {
-	        return dataDiNascita;
-	     } 
-	  }
-	}
-	 
-	public String validaTelefono(String telefono) throws TelefonoNonValidoException {
-		if(telefono == null) throw new TelefonoNonValidoException("Il campo telefono non può essere nullo");
-		
-		if(!telefono.matches(Utente.TELEFONO_PATTERN)) throw new TelefonoNonValidoException("Il campo telefono non è valido");
-		
-		return telefono;
-	}
-		
-	public String validaUsername(String username) throws UsernameNonValidoException, UsernameEsistenteException {
-		if(username == null) throw new UsernameNonValidoException("Il campo username è nullo");
-			
-		else
-		username = username.trim();
-			
-		if(!username.matches(Utente.USERNAME_PATTERN)) throw new UsernameNonValidoException("Lo username non rispetta il formato.");
-			
-		else if(utenteRep.existsByUsername(username)) throw new UsernameEsistenteException("Username già utilizzato.");
-			
-		return username;
-			
-	}
-	
-	public boolean validaPasswords(String password, String confirmPassword) throws PasswordNonValidaException, PasswordNonCorrispondentiException {
-		if(password == null || confirmPassword == null) throw new PasswordNonValidaException("Il campo password oppure il campo conferma password sono nulli");
-		
-		if(!password.matches(Utente.PASSWORD_PATTERN) || !confirmPassword.matches(Utente.PASSWORD_PATTERN)) throw new PasswordNonValidaException("Il campo password oppure il campo conferma password non rispettano il formato previsto");
-		
-		if(!password.equals(confirmPassword)) throw new PasswordNonCorrispondentiException("Le due password non corrispondono");
-		
-		return true;
-	}
-	
-	public String validaPassword(String password) throws PasswordNonValidaException, PasswordNonCorrispondentiException {
-		if(password == null) throw new PasswordNonValidaException("STUDENTE SERVICE - Il campo password non è valido");
-		
-		if(!password.matches(Utente.PASSWORD_PATTERN)) throw new PasswordNonValidaException("STUDENTE SERVICE - Non rispecchia il formato");
-		
-		return password;
-	}
-	  
+  /** 
+  * Costante che rappresenta la massima distanza in anni dalla data corrente 
+  * per la data di nascita.
+  */
+  public static final int MAX_DISTANZA_ANNO_NASCITA = 130;
+
+  @Transactional(rollbackFor = Exception.class)
+  public Studente registraStudente(Studente studente) throws NomeNonValidoException,
+      NomeCognomeTroppoLungoException, NomeCognomeTroppoCortoException, CognomeNonValidoException,
+      DataDiNascitaNonValidaException, UsernameNonValidoException, 
+      UsernameEsistenteException, EmailNonValidaException, 
+      EmailEsistenteException, SessoNonValidoException, TelefonoNonValidoException,
+      MatricolaStudenteNonValidaException, MatricolaStudenteEsistenteException,
+      PasswordNonValidaException, PasswordNonCorrispondentiException {
+
+    studente.setNome(validaNome(studente.getNome()));
+    studente.setCognome(validaCognome(studente.getCognome()));
+    studente.setDataDiNascita(validaDataDiNascita(studente.getDataDiNascita()));
+    studente.setUsername(validaUsername(studente.getUsername()));
+    studente.setEmail(validaEmailStudente(studente.getEmail()));
+    studente.setSesso(validaSesso(studente.getSesso()));
+    studente.setTelefono(validaTelefono(studente.getTelefono()));
+    studente.setMatricola(validaMatricolaStudente(studente.getMatricola()));
+    studente.setPassword(validaPassword(studente.getPassword()));
+
+    studente = studenteRepository.save(studente);
+    
+    return studente;
+
+  }
+
+
+  public String validaMatricolaStudente(String matricolaStudente)
+      throws MatricolaStudenteNonValidaException, MatricolaStudenteEsistenteException {
+    if (matricolaStudente == null) {
+
+      throw new MatricolaStudenteNonValidaException();
+
+    } else {
+
+      matricolaStudente = matricolaStudente.trim();
+
+      if (!matricolaStudente.matches(Studente.MATRICOLA_PATTERN)) {
+        throw new MatricolaStudenteNonValidaException();
+
+      } else if (studenteRepository.existsByMatricola(matricolaStudente)) {
+        throw new MatricolaStudenteEsistenteException();
+
+      } else {
+
+        return matricolaStudente;
+      }
+    }
+  }
+
+  public String validaEmailStudente(String email) throws EmailNonValidaException,
+      EmailEsistenteException {
+    if (email == null) {
+      throw new EmailNonValidaException("Il campo email non può essere nullo");
+    }
+
+    if (!email.matches(Studente.EMAIL_PATTERN_STUDENTE)) {
+      throw new EmailNonValidaException();
+    }
+
+    if (studenteRepository.existsByEmail(email)) {
+      throw new EmailEsistenteException("L'email è già utilizzata da un altro studente");
+    }
+
+    return email;
+  }
+
+
+  public String validaNome(String nome) throws NomeNonValidoException, 
+      NomeCognomeTroppoLungoException, NomeCognomeTroppoCortoException {
+    if (nome == null) {
+      throw new NomeNonValidoException();
+    }
+
+    if (nome.length() > Utente.MAX_LUNGHEZZA_NOME) {
+      throw new NomeCognomeTroppoLungoException();
+    }
+
+    if (nome.length() < Utente.MIN_LUNGHEZZA_NOME) {
+      throw new NomeCognomeTroppoCortoException();
+    }
+
+    if (!nome.matches(Utente.NOME_PATTERN)) {
+      throw new NomeNonValidoException("Il campo nome non rispetta il formato indicato");
+    }
+
+    return nome;
+  }
+
+
+  public String validaCognome(String cognome) throws CognomeNonValidoException, 
+      NomeCognomeTroppoLungoException, NomeCognomeTroppoCortoException {
+    if (cognome == null) {
+      throw new CognomeNonValidoException();
+    }
+
+    if (cognome.length() > Utente.MAX_LUNGHEZZA_NOME) {
+      throw new NomeCognomeTroppoLungoException();
+    }
+
+    if (cognome.length() < Utente.MIN_LUNGHEZZA_NOME) {
+      throw new NomeCognomeTroppoCortoException();
+    }
+
+    if (!cognome.matches(Utente.COGNOME_PATTERN)) {
+      throw new CognomeNonValidoException("Il cognome non rispetta il formato indicato");
+    }
+
+    return cognome;
+  }
+
+
+  public String validaSesso(String sesso) throws SessoNonValidoException {
+    if (sesso == null) {
+      throw new SessoNonValidoException();
+
+    } else {
+      sesso = sesso.trim();
+
+      if (!sesso.equals(Utente.SESSO_MASCHILE)
+          && !sesso.equals(Utente.SESSO_FEMMINILE)) {
+        throw new SessoNonValidoException();
+      } else {
+        return sesso;
+      }
+    }
+  }
+
+
+  public LocalDate validaDataDiNascita(LocalDate dataDiNascita) 
+      throws DataDiNascitaNonValidaException {
+
+    if (dataDiNascita == null) {
+      throw new DataDiNascitaNonValidaException();
+
+    } else {
+
+      LocalDate oggi = LocalDate.now();
+      long distanza = ChronoUnit.YEARS.between(dataDiNascita, oggi);
+
+      if (distanza < MIN_DISTANZA_ANNO_NASCITA || distanza > MAX_DISTANZA_ANNO_NASCITA) {
+        throw new DataDiNascitaNonValidaException();
+      } else {
+        return dataDiNascita;
+      } 
+    }
+  }
+
+  public String validaTelefono(String telefono) throws TelefonoNonValidoException {
+    if (telefono == null) {
+      throw new TelefonoNonValidoException("Il campo telefono non può essere nullo");
+    }
+
+    if (!telefono.matches(Utente.TELEFONO_PATTERN)) {
+      throw new TelefonoNonValidoException("Il campo telefono non è valido");
+    }
+
+    return telefono;
+  }
+
+  public String validaUsername(String username) throws UsernameNonValidoException,
+      UsernameEsistenteException {
+    if (username == null) {
+      throw new UsernameNonValidoException("Il campo username è nullo");
+    } else {
+      username = username.trim();
+
+      if (!username.matches(Utente.USERNAME_PATTERN)) {
+        throw new UsernameNonValidoException("Lo username non rispetta il formato.");
+      }
+
+    }
+    if (utenteRep.existsByUsername(username)) {
+      throw new UsernameEsistenteException("Username già utilizzato.");
+    }
+
+    return username;
+
+  }
+
+  public boolean validaPasswords(String password, String confirmPassword)
+      throws PasswordNonValidaException,PasswordNonCorrispondentiException {
+    if (password == null || confirmPassword == null) {
+      throw new PasswordNonValidaException(
+     "Il campo password oppure il campo conferma password sono nulli");
+    }
+
+    if (!password.matches(Utente.PASSWORD_PATTERN) 
+        || !confirmPassword.matches(Utente.PASSWORD_PATTERN)) {
+      throw new PasswordNonValidaException(
+      "Il campo password oppure il campo conferma password non rispettano il formato previsto");
+    }
+
+    if (!password.equals(confirmPassword)) {
+      throw new PasswordNonCorrispondentiException("Le due password non corrispondono");
+    }
+
+    return true;
+  }
+
+  public String validaPassword(String password) 
+      throws PasswordNonValidaException, PasswordNonCorrispondentiException {
+    if (password == null) {
+      throw new PasswordNonValidaException("STUDENTE SERVICE - Il campo password non è valido");
+    }
+
+    if (!password.matches(Utente.PASSWORD_PATTERN)) {
+      throw new PasswordNonValidaException("STUDENTE SERVICE - Non rispecchia il formato");
+    }
+
+    return password;
+  }
+
 }
--- ittraining/src/main/java/it/unisa/di/ittraining/tutoraccademico/TutorAccademico.java
+++ ittraining/src/main/java/it/unisa/di/ittraining/tutoraccademico/TutorAccademico.java
@@ -12,36 +12,38 @@ import it.unisa.di.ittraining.utente.Utente;
 
 @Entity
 public class TutorAccademico extends Utente {
-	  
-	/** Espressione regolare che definisce il formato del campo email per impiegati di segreteria e tutor accademici. */
-	public static final String EMAIL_PATTERN_ACCADEMICO = "[A-z0-9\\.\\+_-]+@unisa.it";
-	
-	@OneToMany
-	private List<Studente> studenti;
-	
-	public TutorAccademico() {
-		this.studenti = new ArrayList<>();
-	}
 
-	public List<Studente> getStudenti() {
-		return studenti;
-	}
+  /** Espressione regolare che definisce il formato del campo email per 
+   * impiegati di segreteria e tutor accademici. */
+  public static final String EMAIL_PATTERN_ACCADEMICO = "[A-z0-9\\.\\+_-]+@unisa.it";
 
-	public void addStudente(Studente studente) {
-		if(!studenti.contains(studente)) {
-			studenti.add(studente);
-			studente.setTutor(this);
-		}
-	}
-	
-	public List<DomandaTirocinio> getAllDomande() {
-		List<DomandaTirocinio> newList = new ArrayList<>();
-		
-		for(Studente s: studenti) 
-			newList.addAll(s.getDomandeTirocinio());
-		
-		return newList;
-	}
-	
-	
+  @OneToMany
+  private List<Studente> studenti;
+
+  public TutorAccademico() {
+    this.studenti = new ArrayList<>();
+  }
+
+  public List<Studente> getStudenti() {
+    return studenti;
+  }
+
+  public void addStudente(Studente studente) {
+    if (!studenti.contains(studente)) {
+      studenti.add(studente);
+      studente.setTutor(this);
+    }
+  }
+
+  public List<DomandaTirocinio> getAllDomande() {
+    List<DomandaTirocinio> newList = new ArrayList<>();
+
+    for (Studente s: studenti) {
+      newList.addAll(s.getDomandeTirocinio());
+    }
+
+    return newList;
+  }
+
+
 }
