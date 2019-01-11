61806b0bdf8f7783426387437b5f51ac753ea42c
Antonio_Basileo <antonio.basileo92@gmail.com>
2019-01-11 20:05:19 +0100

    Checkstyle: parte 4

--- ittraining/src/main/java/it/unisa/di/ittraining/web/ProgettoFormativoForm.java
+++ ittraining/src/main/java/it/unisa/di/ittraining/web/ProgettoFormativoForm.java
@@ -1,20 +1,23 @@
 package it.unisa.di.ittraining.web;
 
 public class ProgettoFormativoForm {
-	
-	public long getIdDomanda() {
-		return idDomanda;
-	}
-	public void setIdDomanda(Long idDomanda) {
-		this.idDomanda = idDomanda;
-	}
-	public String getDescrizione() {
-		return descrizione;
-	}
-	public void setDescrizione(String descrizione) {
-		this.descrizione = descrizione;
-	}
-	
-	private long idDomanda;
-	private String descrizione;
+
+  public long getIdDomanda() {
+    return idDomanda;
+  }
+
+  public void setIdDomanda(Long idDomanda) {
+    this.idDomanda = idDomanda;
+  }
+
+  public String getDescrizione() {
+    return descrizione;
+  }
+
+  public void setDescrizione(String descrizione) {
+    this.descrizione = descrizione;
+  }
+
+  private long idDomanda;
+  private String descrizione;
 }
